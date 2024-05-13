f801b9d5b216b7cc1bbfde2517a196692fa59200
justin-oestmann <justin.oestmann@gmail.com>
2024-05-13 21:54:00 +0200

    Updated to static Connection

--- server/src/main/java/com/sejacha/server/Database.java
+++ server/src/main/java/com/sejacha/server/Database.java
@@ -5,34 +5,42 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Database {
-    private Connection connection;
+    private static Connection connection;
 
-    // Konstruktor
-    public Database(String host, int port, String databaseName, String username, String password) {
-        String url = "jdbc:mysql://" + host + ":" + port + "/" + databaseName;
-
-        try {
-            // Verbindung zur Datenbank herstellen
-            this.connection = DriverManager.getConnection(url, username, password);
-            System.out.println("Verbindung zur MySQL-Datenbank hergestellt");
-        } catch (SQLException e) {
-            System.err.println("Fehler beim Verbinden mit der Datenbank: " + e.getMessage());
-        }
+    // Private Konstruktor, um Instanziierung zu verhindern
+    private Database() {
+        // Leerer Konstruktor
     }
 
-    // Methode zur Rückgabe der Datenbankverbindung
-    public Connection getConnection() {
-        return this.connection;
-    }
+    // Statische Methode zur Rückgabe der Datenbankverbindung
+    public static Connection getConnection() {
+        if (connection == null) {
+            String url = "jdbc:mysql://" + Config.getConfig("mysql.server") + ":" + Config.getConfig("mysql.port") + "/"
+                    + Config
+                            .getConfig("mysql.database");
 
-    // Methode zur Schließung der Datenbankverbindung
-    public void closeConnection() {
-        if (this.connection != null) {
             try {
-                this.connection.close();
+                // Verbindung zur Datenbank herstellen
+                connection = DriverManager.getConnection(url, Config.getConfig("mysql.user"),
+                        Config.getConfig("mysql.password"));
+                System.out.println("Verbindung zur MySQL-Datenbank hergestellt");
+            } catch (SQLException e) {
+                System.err.println("Fehler beim Verbinden mit der Datenbank: " + e.getMessage());
+            }
+        }
+        return connection;
+    }
+
+    // Statische Methode zum Schließen der Datenbankverbindung
+    public static void closeConnection() {
+        if (connection != null) {
+            try {
+                connection.close();
                 System.out.println("Verbindung zur MySQL-Datenbank geschlossen");
             } catch (SQLException e) {
                 System.err.println("Fehler beim Schließen der Datenbankverbindung: " + e.getMessage());
+            } finally {
+                connection = null; // Setze die Connection-Referenz zurück
             }
         }
     }
