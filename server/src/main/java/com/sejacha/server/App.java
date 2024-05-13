2b18bc47cfb222bcc12251a27dd33f05b3646f87
justin-oestmann <justin.oestmann@gmail.com>
2024-05-13 23:13:53 +0200

    Changed IP-Adress

--- server/src/main/java/com/sejacha/server/App.java
+++ server/src/main/java/com/sejacha/server/App.java
@@ -1,1 +1,21 @@
-//
+package com.sejacha.server;
+
+import java.sql.*;
+import com.sejacha.server.Database;
+
+class App {
+    public static void main(String[] args) throws SQLException {
+        System.out.println("starting...");
+
+        System.out.println("Database:" + Config.getConfig("mysql.server"));
+
+        Statement stmt = Database.getConnection().createStatement();
+        ResultSet rs = stmt.executeQuery("SELECT * FROM users");
+
+        while (rs.next()) {
+            int id = rs.getInt("id");
+            String name = rs.getString("name");
+            System.out.println("ID: " + id + ", Name: " + name);
+        }
+    }
+}
