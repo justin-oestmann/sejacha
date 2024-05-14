ac1594d016f2c7bc0920905583aad64ac09b1c0f
justin-oestmann <justin.oestmann@gmail.com>
2024-05-14 16:59:14 +0200

    Updated Main.java

--- client/src/main/java/com/sejacha/client/Main.java
+++ client/src/main/java/com/sejacha/client/Main.java
@@ -1,10 +1,31 @@
 package com.sejacha.client;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) throws Exception {
-        SocketClient serverSocket = new SocketClient("127.0.0.1", 4999);
-        serverSocket.connect();
-        serverSocket.sendData("test123");
+        String serverAddress = "84.252.120.50";
+        int serverPort = 4999;
+        System.out.println("Server:" + serverAddress + ":" + serverPort);
+        System.out.println("Connecting...");
+        SocketClient client = new SocketClient(serverAddress, serverPort);
 
+        // Send messages to the server
+
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
+            String data = null;
+            data = scanner.nextLine();
+            if (data != null) {
+                switch (data) {
+                    case "pong":
+                        client.sendMessage("\"exec\" : \"send\", \"data\": \"test\"");
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+        }
     }
 }
--- server/src/main/java/com/sejacha/server/Main.java
+++ server/src/main/java/com/sejacha/server/Main.java
@@ -1,13 +1,31 @@
 package com.sejacha.server;
 
+import java.io.IOException;
+import java.net.ServerSocket;
 import java.sql.*;
+import java.util.Scanner;
 
 class Main {
     public static void main(String[] args) throws SQLException, Exception {
-        System.out.println("starting socket...");
 
-        SocketServer serverSocket = new SocketServer();
-        serverSocket.start();
+        System.out.println("Starting Server...");
+
+        SocketServer ss = new SocketServer(4999, true);
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String data = scanner.nextLine();
+            if (data == "ping") {
+                for (ServerClient client : ss.getClients()) {
+                    client.sendMessage("ping!");
+                }
+            }
+            if (data == "close") {
+                scanner.close();
+                System.exit(0);
+            }
+        }
 
     }
 }
