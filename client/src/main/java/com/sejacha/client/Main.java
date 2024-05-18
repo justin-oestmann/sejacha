911766793d104181a2043bd92cf8dd71cbcc3aa5
justin-oestmann <justin.oestmann@gmail.com>
2024-05-18 16:48:32 +0200

    Updated Command-Handling

--- client/src/main/java/com/sejacha/client/Main.java
+++ client/src/main/java/com/sejacha/client/Main.java
@@ -2,9 +2,11 @@ package com.sejacha.client;
 
 import java.util.Scanner;
 
+import com.sejacha.client.SysPrinter;
+
 public class Main {
     public static void main(String[] args) throws Exception {
-        String serverAddress = "84.252.120.50";
+        String serverAddress = "127.0.0.1";
         int serverPort = 4999;
         System.out.println("Server:" + serverAddress + ":" + serverPort);
         System.out.println("Connecting...");
@@ -15,14 +17,17 @@ public class Main {
         while (true) {
             Scanner scanner = new Scanner(System.in);
             String data = null;
+            System.out.print(">");
             data = scanner.nextLine();
             if (data != null) {
                 switch (data) {
                     case "pong":
-                        client.sendMessage("\"exec\" : \"send\", \"data\": \"test\"");
+                        client.sendMessage("{\"exec\" : \"send\", \"data\": \"test\"}");
+                        SysPrinter.println("Info", "Command sent!");
                         break;
 
                     default:
+                        SysPrinter.println("Error", "Command not found!");
                         break;
                 }
             }
