555428c44b73d1e24d69fde6f88b3c775a83a963
justin-oestmann <justin.oestmann@gmail.com>
2024-05-18 17:49:21 +0200

    Updated User-Credentials Getter

--- client/src/main/java/com/sejacha/client/Main.java
+++ client/src/main/java/com/sejacha/client/Main.java
@@ -1,16 +1,49 @@
 package com.sejacha.client;
 
+import java.io.Console;
 import java.util.Scanner;
 
 import com.sejacha.client.SysPrinter;
 
 public class Main {
     public static void main(String[] args) throws Exception {
+
         String serverAddress = "127.0.0.1";
         int serverPort = 4999;
         System.out.println("Server:" + serverAddress + ":" + serverPort);
         System.out.println("Connecting...");
         SocketClient client = new SocketClient(serverAddress, serverPort);
+
+        String authToken = "";
+
+        while (authToken == "") {
+            String username = null;
+            String password = null;
+
+            Scanner scanner1 = new Scanner(System.in);
+            System.out.println("Please enter your username:");
+
+            System.out.print("> ");
+
+            username = scanner1.nextLine();
+            scanner1.close();
+
+            Scanner scanner2 = new Scanner(System.in);
+            System.out.println("Please enter your password:");
+
+            System.out.print("> ");
+
+            password = scanner2.nextLine();
+
+            scanner2.close();
+
+            if (username != "" && password != "") {
+
+                client.sendMessage(
+                        "{\"exec\":\"auth\", \"username\":\"" + username + "\", \"password\":\"" + password + "\"}");
+                SysPrinter.println("Info", "Checking credentials...");
+            }
+        }
 
         // Send messages to the server
 
