bc1a7e4e0be4bbf5ad8296af3c3b5280f5a9bb7c
justin-oestmann <justin.oestmann@gmail.com>
2024-05-19 14:53:54 +0200

    added auth function to User

--- server/src/main/java/com/sejacha/server/User.java
+++ server/src/main/java/com/sejacha/server/User.java
@@ -1,6 +1,11 @@
 
 package com.sejacha.server;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.format.DateTimeFormatter;
+
 public class User {
 
     private String id;
@@ -8,5 +13,30 @@ public class User {
     private String email;
     private String password;
     private Boolean state;
+    private Boolean auth;
+
+    public boolean auth(String username, String passwordHash) throws Exception {
+
+        String query = "SELECT * FROM users WHERE user_name = ? AND user_password = ?";
+        PreparedStatement stmt = Database.getConnection().prepareStatement(query);
+
+        stmt.setString(1, username);
+        stmt.setString(2, passwordHash);
+
+        ResultSet rs = stmt.executeQuery();
+
+        if (rs.next() && rs.getString("user_name") == username && rs.getString("user_password") == passwordHash) {
+            this.auth = true;
+            // return true;
+        } else {
+            // return false;
+        }
+
+        // SysPrinter.println("info!", rs.getString("user_name"));
+        // SysPrinter.println("info!", rs.getString("user_password"));
+        // SysPrinter.println("info!", passwordHash);
+
+        return false;
+    }
 
 }
