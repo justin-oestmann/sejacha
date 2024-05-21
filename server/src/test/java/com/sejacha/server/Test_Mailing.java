224dc20158c867baacc35d44989462e1e210f4b1
justin-oestmann <justin.oestmann@gmail.com>
2024-05-21 11:25:09 +0200

    added/updated test-classes

---
+++ server/src/test/java/com/sejacha/server/Test_Mailing.java
@@ -0,0 +1,18 @@
+package com.sejacha.server;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+import javax.mail.MessagingException;
+
+public class Test_Mailing {
+
+    // @Test
+    // @DisplayName("TestMail")
+    // public void sendTestMail() throws MessagingException {
+    // Mailing.sendEmail("justin.oestmann@gmail.com", "TestMail", "Info!");
+    // }
+
+}
--- server/src/test/java/com/sejacha/server/Test_Message.java
+++ server/src/test/java/com/sejacha/server/Test_Message.java
@@ -7,16 +7,16 @@ import java.time.LocalDateTime;
 
 public class Test_Message {
 
-    @Test
-    @DisplayName("CreateTestMessage")
-    public void createTestMessage() {
-        Message msg = new Message();
+    // @Test
+    // @DisplayName("CreateTestMessage")
+    // public void createTestMessage() {
+    // Message msg = new Message();
 
-        msg.setUserID("null");
-        msg.setRoomID("null");
-        msg.setTimeCreated();
-        msg.setTimeCreated(LocalDateTime.now());
-        msg.setMessage("null");
-    }
+    // msg.setUserID("null");
+    // msg.setRoomID("null");
+    // msg.setTimeCreated();
+    // msg.setTimeCreated(LocalDateTime.now());
+    // msg.setMessage("null");
+    // }
 
 }
