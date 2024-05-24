91e40fd3b4e6456415abaff137392ab85c567e41
justin-oestmann <justin.oestmann@gmail.com>
2024-05-24 17:23:15 +0200

    Updated SysPrinter Functions

--- server/src/main/java/com/sejacha/server/SysPrinter.java
+++ server/src/main/java/com/sejacha/server/SysPrinter.java
@@ -1,5 +1,6 @@
 package com.sejacha.server;
 
+import java.net.Socket;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
@@ -7,6 +8,18 @@ public class SysPrinter {
     public static void println(String source, String message) {
         String time = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
         System.out.println(time + " | " + source + " |-> " + message);
+        return;
+    }
+
+    public static void println(Socket socket, String source, String message) {
+        String time = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+        System.out.println(time + " | " + socket.getPort() + " " + source + " |-> " + message);
+        return;
+    }
+
+    public static void println(Exception exception) {
+        String time = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+        System.out.println(time + " | EXCEPTION! |-> " + exception.getMessage());
         return;
     }
 }
