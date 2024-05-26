7e01fa2bc8fbe29e747f40074d8a622aa839dd80
yeetqze <luca23.email+github@gmail.com>
2024-05-26 20:34:41 +0200

    Updatet: createRoom() Created: getter / setter

--- server/src/main/java/com/sejacha/server/Room.java
+++ server/src/main/java/com/sejacha/server/Room.java
@@ -5,83 +5,137 @@ import java.sql.SQLException;
 import java.time.LocalDateTime;
 
 public class Room {
-    public String id;
+    public String id = null;
     public String owner;
     public String name;
-    public int type = 0;    // 0 - Public (Standard) / 1 - Private
+    public RoomType type = RoomType.PUBLIC;
     public String password;
     public LocalDateTime timestamp;
 
-    public Room (){
+    public Room() {
 
     }
 
     /**
      * Creats a Room if an ID is not already set
+     * 
      * @param owner String owner of the Room
-     * @param name String name of the Room
+     * @param name  String name of the Room
      * @return boolean true = could be created / false = couldn't be created
      * @throws Exception
      */
-    public boolean createRoom(String owner, String name) throws Exception{
-        if (!(this.id != null)){
-            this.id = Database.getUniqueID("room");
-            this.owner = owner;
-            this.name = name;
-
-            return true;
+    public boolean createRoom(String owner, String name) throws Exception {
+        if (this.id != null) {
+            return false;
         }
-        
 
-        return false;
+        this.id = Database.getUniqueID("room");
+        this.owner = owner;
+        this.name = name;
+
+        return true;
     }
 
     /**
      * Creats a Room if an ID is not already set
+     * 
      * @param owner String owner of the Room
-     * @param name String name of the Room
-     * @param type int declares the type of the Room (0 - Public (Standard) / 1 - Private)
+     * @param name  String name of the Room
+     * @param type  int declares the type of the Room (0 - Public (Standard) / 1 -
+     *              Private)
      * @return boolean true = could be created / false = couldn't be created
      * @throws Exception
      */
-    public boolean createRoom(String owner, String name, int type) throws Exception{
-        if (this.id.equals(null)){
-            if (type != 1) {
-                this.id = Database.getUniqueID("room");
-                this.owner = owner;
-                this.name = name;
-                this.type = type;
-    
-                return true;
-            }
+    public boolean createRoom(String owner, String name, RoomType type) throws Exception {
+        if (this.id != null) {
+            return false;
         }
-        
 
-        return false;
+        if (type == RoomType.PRIVATE) {
+            return false;
+        }
+
+        this.id = Database.getUniqueID("room");
+        this.owner = owner;
+        this.name = name;
+        this.type = type;
+
+        return true;
     }
 
     /**
      * Creats a private Room if an ID is not already set
-     * @param owner String owner of the Room
-     * @param name String name of the Room
+     * 
+     * @param owner    String owner of the Room
+     * @param name     String name of the Room
      * @param password String password of the private Room
      * @return
      * @throws Exception
      */
-    public boolean createRoom(String owner, String name, String password) throws Exception{
-        if (!(this.id != null)){
-            
-            this.id = Database.getUniqueID("room");
-            this.owner = owner;
-            this.name = name;
-            this.type = 1;
-            this.password = password;
-    
-            return true;
-        
+    public boolean createRoom(String owner, String name, String password) throws Exception {
+        if (this.id != null) {
+            return false;
         }
-        
 
-        return false;
+        this.id = Database.getUniqueID("room");
+        this.owner = owner;
+        this.name = name;
+        this.type = RoomType.PRIVATE;
+        this.password = password;
+
+        return true;
     }
+
+    // getter
+
+    public String getID() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public boolean validatePassword(String password) {
+        return this.password == password;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public RoomType getType() {
+        return type;
+    }
+
+    // setter
+
+    private void setID(String id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public void setType(RoomType type) {
+        this.type = type;
+    }
+
 }
