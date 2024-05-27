c64af23b3a5d41118a7f0d28239e009641ec4289
UniqoHazarD <arnsnico228@gmail.com>
2024-05-27 12:28:31 +0200

    Revert "Deleted Room out of ChatHandler"
    This reverts commit 835c04989643e6cb0827536f293264b17b240038.

--- client/src/main/java/com/sejacha/client/ChatHandler.java
+++ client/src/main/java/com/sejacha/client/ChatHandler.java
@@ -9,6 +9,47 @@ import java.util.Properties;
 import java.util.Scanner;
 import java.sql.*;
 
+class Room {
+    private int id;
+    private String name;
+    private String owner;
+    private int type;
+    private String password;
+    private LocalDateTime timestamp;
+
+    public Room(int id, String name, String owner, int type, String password, LocalDateTime timestamp) {
+        this.id = id;
+        this.name = name;
+        this.owner = owner;
+        this.type = type;
+        this.password = password;
+        this.timestamp = timestamp;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+}
 
 public class ChatHandler {
     private String[] commands = {"room", "help", "login", "register"};
