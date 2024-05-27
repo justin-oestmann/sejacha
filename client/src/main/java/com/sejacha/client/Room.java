bde51ebdea4b81495f912afb56c6733e2c8ffe1a
UniqoHazarD <arnsnico228@gmail.com>
2024-05-27 11:54:59 +0200

    Added Room.java to Client

---
+++ client/src/main/java/com/sejacha/client/Room.java
@@ -0,0 +1,45 @@
+package com.sejacha.client;
+
+import java.time.LocalDateTime;
+
+public class Room {
+    private int id;
+    private String name;
+    private String owner;
+    private int type;
+    private String password;
+    private LocalDateTime timestamp;
+
+    public Room(int id, String name, String owner, int type, String password, LocalDateTime timestamp) {
+        this.id = id;
+        this.name = name;
+        this.owner = owner;
+        this.type = type;
+        this.password = password;
+        this.timestamp = timestamp;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+}
