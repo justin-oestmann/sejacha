a4fa05a69768566a7b09eb6e90973bd7dfc3e919
UniqoHazarD <arnsnico228@gmail.com>
2024-05-28 10:33:37 +0200

    Added RegisterFail

--- client/src/main/java/com/sejacha/client/SocketClientResponse.java
+++ client/src/main/java/com/sejacha/client/SocketClientResponse.java
@@ -4,4 +4,6 @@ interface SocketClientResponse {
     void onLoginSuccess(SocketMessage response);
 
     void onLoginFail(SocketMessage response);
+
+    void onRegisterSuccess(SocketMessage response);
 }
