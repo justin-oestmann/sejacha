bbe922f683e13ccd93bfd013af9f76d59f5ac453
justin-oestmann <justin.oestmann@gmail.com>
2024-05-28 22:38:30 +0200

    Added SocketMessageTypes as Eventhandlerfunctions

--- client/src/main/java/com/sejacha/client/SocketClientResponse.java
+++ client/src/main/java/com/sejacha/client/SocketClientResponse.java
@@ -8,4 +8,49 @@ interface SocketClientResponse {
     void onRegisterSuccess(SocketMessage response);
 
     void onRegisterFail(SocketMessage response);
+
+    void onLogoutSuccess(SocketMessage response);
+
+    void onLogoutFail(SocketMessage response);
+
+    void onNewMessageSuccess(SocketMessage response);
+
+    void onNewMessageFail(SocketMessage response);
+
+    void onRoomJoinSuccess(SocketMessage response);
+
+    void onRoomJoinFail(SocketMessage response);
+
+    void onRoomJoinWPasswordSuccess(SocketMessage response);
+
+    void onRoomJoinWPasswordFail(SocketMessage response);
+
+    void onRoomLeaveSuccess(SocketMessage response);
+
+    void onRoomLeaveFail(SocketMessage response);
+
+    void onRoomCreateSuccess(SocketMessage response);
+
+    void onRoomCreateFail(SocketMessage response);
+
+    void onRoomGetInfoSuccess(SocketMessage response);
+
+    void onRoomGetInfoFail(SocketMessage response);
+
+    void onRoomInviteContactSuccess(SocketMessage response);
+
+    void onRoomInviteContactFail(SocketMessage response);
+
+    void onContactAddSuccess(SocketMessage response);
+
+    void onContactAddFail(SocketMessage response);
+
+    void onContactRemoveSuccess(SocketMessage response);
+
+    void onContactRemoveFail(SocketMessage response);
+
+    void onContactCreateDMRoomSuccess(SocketMessage response);
+
+    void onContactCreateDMRoomFail(SocketMessage response);
+
 }
