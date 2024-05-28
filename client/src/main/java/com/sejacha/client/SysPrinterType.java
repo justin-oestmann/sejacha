953435e45172283351594f229a560cf098a5859b
UniqoHazarD <arnsnico228@gmail.com>
2024-05-28 10:24:00 +0200

    Added SysPrinterType

---
+++ client/src/main/java/com/sejacha/client/SysPrinterType.java
@@ -0,0 +1,27 @@
+package com.sejacha.client;
+
+public enum SysPrinterType {
+
+    INFO("INFO"),
+    ERROR("ERROR"),
+    WARNING("WARNING");
+
+    private final String nameOfType;
+
+    SysPrinterType(String nameOfType) {
+        this.nameOfType = nameOfType;
+    }
+
+    public String getNameOfType() {
+        return nameOfType;
+    }
+
+    public static SysPrinterType fromString(String text) {
+        for (SysPrinterType type : SysPrinterType.values()) {
+            if (type.nameOfType.equalsIgnoreCase(text)) {
+                return type;
+            }
+        }
+        throw new IllegalArgumentException("No enum constant with text " + text);
+    }
+}
