afe8398d9ab7bdab788faa178c9fb33e7d6e365b
HolzVanHorst <mike@kluth-bochum.de>
2024-05-30 22:41:22 +0200

    languageText javadok

--- server/src/main/java/com/sejacha/server/LanguageText.java
+++ server/src/main/java/com/sejacha/server/LanguageText.java
@@ -1,18 +1,39 @@
 package com.sejacha.server;
 
+/**
+ * Enum representing various language texts used in the application.
+ */
 public enum LanguageText {
     EMAIL_VERIFY_SUBJECT("email.verify_mail_subject");
 
     private final String nameOfType;
 
+    /**
+     * Constructs a new LanguageText enum constant with the specified name.
+     *
+     * @param nameOfType the name of the language text
+     */
     LanguageText(String nameOfType) {
         this.nameOfType = nameOfType;
     }
 
+    /**
+     * Retrieves the name of the language text.
+     *
+     * @return the name of the language text
+     */
     public String getNameOfType() {
         return nameOfType;
     }
 
+    /**
+     * Retrieves the LanguageText enum constant associated with the specified text.
+     *
+     * @param text the text associated with the enum constant
+     * @return the LanguageText enum constant
+     * @throws IllegalArgumentException if no enum constant is found with the
+     *                                  specified text
+     */
     public static LanguageText fromString(String text) {
         for (LanguageText type : LanguageText.values()) {
             if (type.nameOfType.equalsIgnoreCase(text)) {
@@ -21,5 +42,4 @@ public enum LanguageText {
         }
         throw new IllegalArgumentException("No enum constant with text " + text);
     }
-
 }
