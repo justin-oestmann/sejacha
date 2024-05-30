5041038db026feb126815c2744f329de8308efee
HolzVanHorst <mike@kluth-bochum.de>
2024-05-30 22:42:36 +0200

    socketMessagesNotNewException javadok

--- server/src/main/java/com/sejacha/server/exceptions/SocketMessageIsNotNewException.java
+++ server/src/main/java/com/sejacha/server/exceptions/SocketMessageIsNotNewException.java
@@ -1,8 +1,19 @@
 package com.sejacha.server.exceptions;
 
+/**
+ * This exception is thrown when a socket message is not in a new state.
+ */
 public class SocketMessageIsNotNewException extends Exception {
+
+    /**
+     * Constructs a new SocketMessageIsNotNewException with the specified detail
+     * message.
+     *
+     * @param message the detail message (which is saved for later retrieval by the
+     *                getMessage() method)
+     */
     public SocketMessageIsNotNewException(String message) {
-        // Rufe den Konstruktor der Superklasse auf und übergebe die Nachricht
+        // Call the constructor of the superclass and pass the message
         super(message);
     }
 }
