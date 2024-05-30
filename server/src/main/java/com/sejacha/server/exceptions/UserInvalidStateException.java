65d10fa21197c7d7fbcee0f3c7859084d9ac6668
HolzVanHorst <mike@kluth-bochum.de>
2024-05-30 22:43:06 +0200

    userinvalidStateException javadok

--- server/src/main/java/com/sejacha/server/exceptions/UserInvalidStateException.java
+++ server/src/main/java/com/sejacha/server/exceptions/UserInvalidStateException.java
@@ -1,8 +1,18 @@
 package com.sejacha.server.exceptions;
 
+/**
+ * This exception is thrown when a user is in an invalid state.
+ */
 public class UserInvalidStateException extends Exception {
+
+    /**
+     * Constructs a new UserInvalidStateException with the specified detail message.
+     *
+     * @param message the detail message (which is saved for later retrieval by the
+     *                getMessage() method)
+     */
     public UserInvalidStateException(String message) {
-        // Rufe den Konstruktor der Superklasse auf und übergebe die Nachricht
+        // Call the constructor of the superclass and pass the message
         super(message);
     }
 }
