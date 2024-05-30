052956b5b0408c834233aba41e851cd5007fbf30
HolzVanHorst <mike@kluth-bochum.de>
2024-05-30 22:14:48 +0200

    java dok für userState.java

--- server/src/main/java/com/sejacha/server/UserState.java
+++ server/src/main/java/com/sejacha/server/UserState.java
@@ -1,3 +1,7 @@
+/**
+ * The {@code UserState} enum represents the possible states of a user in the system.
+ * Each state is associated with an integer value.
+ */
 package com.sejacha.server;
 
 public enum UserState {
@@ -7,14 +11,32 @@ public enum UserState {
 
     private final int intValue;
 
+    /**
+     * Constructs a {@code UserState} with the specified integer value.
+     *
+     * @param intValue the integer value associated with the user state
+     */
     UserState(int intValue) {
         this.intValue = intValue;
     }
 
+    /**
+     * Returns the integer value associated with this {@code UserState}.
+     *
+     * @return the integer value of this user state
+     */
     public int getNameOfType() {
         return intValue;
     }
 
+    /**
+     * Returns the {@code UserState} corresponding to the specified integer value.
+     *
+     * @param value the integer value of the user state
+     * @return the {@code UserState} corresponding to the specified value
+     * @throws IllegalArgumentException if no user state corresponds to the
+     *                                  specified value
+     */
     public static UserState fromInt(int value) {
         for (UserState enumValue : UserState.values()) {
             if (enumValue.intValue == value) {
@@ -23,5 +45,4 @@ public enum UserState {
         }
         throw new IllegalArgumentException("No enum constant with int value " + value);
     }
-
 }
