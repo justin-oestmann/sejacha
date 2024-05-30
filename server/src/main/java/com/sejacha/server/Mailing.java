036b6788dd8b358ccfdc133fd74b0168852b0700
HolzVanHorst <mike@kluth-bochum.de>
2024-05-30 23:04:25 +0200

    mailing imports hinzugefügt

--- server/src/main/java/com/sejacha/server/Mailing.java
+++ server/src/main/java/com/sejacha/server/Mailing.java
@@ -7,6 +7,9 @@ import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.PasswordAuthentication;
 
 /**
  * Utility class for sending emails.
