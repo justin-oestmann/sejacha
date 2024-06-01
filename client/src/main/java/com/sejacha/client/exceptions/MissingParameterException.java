33a3ac7c79d73cd7322563a0c83916e32b39936b
justin-oestmann <justin.oestmann@gmail.com>
2024-06-02 01:06:28 +0200

    added exceptions

---
+++ client/src/main/java/com/sejacha/client/exceptions/MissingParameterException.java
@@ -0,0 +1,18 @@
+package com.sejacha.client.exceptions;
+
+/**
+ * This exception is thrown when a required parameter is missing or incorrectly
+ * filled.
+ */
+public class MissingParameterException extends Exception {
+
+    /**
+     * Constructs a new MissingParameterException with the specified parameter name.
+     *
+     * @param parameterName the name of the missing or incorrectly filled parameter
+     */
+    public MissingParameterException(String parameterName) {
+        // Call the constructor of the superclass and pass the formatted message
+        super("The parameter " + parameterName + " is missing or not correctly filled");
+    }
+}
---
+++ client/src/main/java/com/sejacha/client/exceptions/SocketMessageIsNotNewException.java
@@ -0,0 +1,19 @@
+package com.sejacha.client.exceptions;
+
+/**
+ * This exception is thrown when a socket message is not in a new state.
+ */
+public class SocketMessageIsNotNewException extends Exception {
+
+    /**
+     * Constructs a new SocketMessageIsNotNewException with the specified detail
+     * message.
+     *
+     * @param message the detail message (which is saved for later retrieval by the
+     *                getMessage() method)
+     */
+    public SocketMessageIsNotNewException(String message) {
+        // Call the constructor of the superclass and pass the message
+        super(message);
+    }
+}
