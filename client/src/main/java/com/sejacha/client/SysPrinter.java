a02fef88e6f9e261f2b17570940f8338e7a1c6ea
justin-oestmann <justin.oestmann@gmail.com>
2024-06-02 01:06:15 +0200

    added new functions (cursor double, clear)

--- client/src/main/java/com/sejacha/client/SysPrinter.java
+++ client/src/main/java/com/sejacha/client/SysPrinter.java
@@ -1,6 +1,23 @@
 package com.sejacha.client;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class SysPrinter {
+
+    protected static boolean cursor_double = false;
+
+    public static void setCursorDouble(boolean state) {
+        cursor_double = state;
+    }
+
+    public static void clear() {
+        System.out.println(" clear ");
+        for (int i = 0; i < 50; ++i) {
+            System.out.println(" ");
+        }
+    }
+
     public static void println(String weight, String message) {
         System.out.println(weight + " | " + message);
         return;
@@ -12,8 +29,22 @@ public class SysPrinter {
     }
 
     public static void printCursor() {
+        if (cursor_double) {
+            System.out.print(">> ");
+            return;
+        }
         System.out.print("> ");
         return;
+    }
+
+    /**
+     * Prints a log message with a timestamp and exception details to the console.
+     *
+     * @param exception the exception to be logged
+     */
+    public static void println(Exception exception) {
+        String time = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+        System.out.println(time + " | EXCEPTION! |-> " + exception.getMessage());
     }
 
 }
