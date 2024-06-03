6f157feba265fac19c4b5d73a00594fbac9b5025
justin-oestmann <justin.oestmann@gmail.com>
2024-06-03 14:08:52 +0200

    fixed room-duplicate bug

--- server/src/main/java/com/sejacha/server/RoomManager.java
+++ server/src/main/java/com/sejacha/server/RoomManager.java
@@ -6,8 +6,10 @@ import java.util.List;
 public class RoomManager {
     private static List<RoomHandler> active_rooms = new ArrayList<>();
 
-    public static void openRoom(RoomHandler roomHandler) {
+    private static void openRoom(RoomHandler roomHandler) {
         active_rooms.add(roomHandler);
+        SysPrinter.println("RoomManager",
+                "opened room " + roomHandler.getRoom().getName() + " (" + roomHandler.getRoom().getID() + ")");
     }
 
     public static boolean joinRoom(ServerClient user, Room room) throws Exception {
@@ -18,8 +20,9 @@ public class RoomManager {
         }
 
         for (RoomHandler roomHandler : active_rooms) {
-            if (roomHandler.getRoom() == room) {
+            if (StringModify.areStringsEqual(roomHandler.getRoom().getID(), room.getID())) {
                 roomHandler.subscribeRoom(user);
+                roomHandler.notifyAll(user.getUser().getName() + " joined the room!");
                 return true;
             }
         }
@@ -29,11 +32,24 @@ public class RoomManager {
 
     public static boolean isRoomActive(Room room) {
         for (RoomHandler roomHandler : active_rooms) {
-            if (roomHandler.getRoom() == room) {
+            SysPrinter.println(roomHandler.getRoom().getID(), room.getID());
+
+            if (StringModify.areStringsEqual(roomHandler.getRoom().getID(), room.getID())) {
+                SysPrinter.println("debug", "true");
                 return true;
             }
+            SysPrinter.println("debug", "false");
         }
         return false;
+    }
+
+    public static RoomHandler getRoomHandler(String room_id) {
+        for (RoomHandler roomHandler : active_rooms) {
+            if (StringModify.areStringsEqual(roomHandler.getRoom().getID(), room_id)) {
+                return roomHandler;
+            }
+        }
+        return null;
     }
 
     public static boolean isRoomActive(RoomHandler room) {
@@ -42,7 +58,7 @@ public class RoomManager {
 
     public static boolean isRoomActive(String room_id) {
         for (RoomHandler roomHandler : active_rooms) {
-            if (roomHandler.getRoom().getID() == room_id) {
+            if (StringModify.areStringsEqual(roomHandler.getRoom().getID(), room_id)) {
                 return true;
             }
         }
